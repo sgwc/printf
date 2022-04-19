4c62bc7d6b99d36ac974513d67c11824d9ef5243
sgwc <45393676+sgwc@users.noreply.github.com>
2022-04-19 21:44:17 +0300

    Update converter.c

--- converter.c
+++ converter.c
@@ -13,9 +13,7 @@ char *convert(unsigned long int num, int base, int lowercase)
     static char buffer[50];
     char *ptr;
 
-    rep = (lowercase)
-? "0123456789abcdef"
-: "0123456789ABCDEF";
+    rep = (lowercase) ? "0123456789abcdef" : "0123456789ABCDEF";
     ptr = &buffer[49];
     *ptr = '\0';
     do
